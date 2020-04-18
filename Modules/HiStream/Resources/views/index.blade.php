689c45f70d4b7646ab08cc7e53793f378e58d938
percy alvarez <percy.alvarez.2017@gmail.com>
2020-04-18 19:06:44 -0400

    delete hi-stream

--- Modules/HiStream/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('histream::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('histream.name') !!}
-    </p>
-@endsection
